42df1e2aea5e96344eb149b38a80f744aeeb5cc3
BitPaw <NozsBitPaw@gmail.com>
2021-12-03 00:52:28 +0100

    UI update

--- Program/UserInterface/UserInterface.cpp
+++ Program/UserInterface/UserInterface.cpp
@@ -1,16 +1,89 @@
 #include "UserInterface.h"
-#include <Network/Server.h>
+#include <wchar.h>
 
-
-UserInterface::UserInterface(QWidget *parent)
-    : QMainWindow(parent)
+UserInterface::UserInterface(QWidget *parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
-    connect(ui.StartButton, &QPushButton::clicked, this, &UserInterface::On_Button_Clicked);
+
+    _server.Callback = this;
+    ui.TextBoxPort->setText("25666");
+    ui.TableClientList->setColumnWidth(0, 10);
+
+    //connect(ui.butt, &QPushButton::clicked, this, &UserInterface::On_Button_Clicked);
+
+    connect(ui.ButtonOpenServer, &QPushButton::clicked, this, &UserInterface::OnButtonOpenServerClicked);
 }
 
-void UserInterface::On_Button_Clicked() {
-    ui.progressBar->setValue(50);
-    ui.BlockLabel->setText(ui.BlocksizeIn->text());
-    ui.treeWidget->currentItem()->setText(0,"worked");
+void UserInterface::OnButtonOpenServerClicked()
+{
+    QString& portText = ui.TextBoxPort->text();
+    unsigned short port = portText.toInt();
+
+    _server.Start(BF::IPVersion::IPVersion4, port);
+}
+
+void UserInterface::OnConnectionLinked(int socketID)
+{
+    char charBuffer[2048];
+
+    sprintf(charBuffer, "[You][%i] Connection Accepted.\n", socketID);
+
+    ui.TextConsole->insertPlainText(charBuffer);
+    
+    
+    QTableWidget& tableWidget = *(ui.TableClientList);
+
+    char textID[255];
+    sprintf(textID, "%i", socketID);
+
+    int rowCount = tableWidget.rowCount();
+
+    tableWidget.insertRow(rowCount);
+    tableWidget.setItem(rowCount, 0, new QTableWidgetItem(textID));
+    tableWidget.setItem(rowCount, 1, new QTableWidgetItem("Client"));
+    tableWidget.setItem(rowCount, 2, new QTableWidgetItem("Conneced"));
+
+   // tableWidget.Width
+}
+
+void UserInterface::OnConnectionListening(int socketID)
+{
+    char charBuffer[2048];
+
+    sprintf(charBuffer, "[You][%i] Connection Listening...\n", socketID);
+
+    ui.TextConsole->insertPlainText(charBuffer);
+}
+
+void UserInterface::OnConnectionEstablished(int socketID)
+{
+    // When Client connect to Server
+    // Does not happen here
+}
+
+void UserInterface::OnConnectionTerminated(int socketID)
+{
+    char charBuffer[2048];
+
+    sprintf(charBuffer, "[Client][%i] Connection Terminated!\n", socketID);
+
+    ui.TextConsole->insertPlainText(charBuffer);
+}
+
+void UserInterface::OnMessageSend(int socketID, const char* message, size_t messageSize)
+{
+    char charBuffer[2048];
+
+    sprintf(charBuffer, "[You][%i] Message:%s (%i Bytes)\n", socketID, message, messageSize);
+
+    ui.TextConsole->insertPlainText(charBuffer);
+}
+
+void UserInterface::OnMessageReceive(int socketID, const char* message, size_t messageSize)
+{
+    char charBuffer[2048];   
+
+    sprintf(charBuffer, "[Client][%i] Message:%s (%i Bytes)\n", socketID, message, messageSize);
+
+    ui.TextConsole->insertPlainText(charBuffer);
 }
--- Program/UserInterface/UserInterface.h
+++ Program/UserInterface/UserInterface.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <Network/Server.h>
+#include <Network/ISocketListener.h>
+
 #include "ui_UserInterface.h"
 
-class UserInterface : public QMainWindow
+class UserInterface : public QMainWindow, public BF::ISocketListener
 {
     Q_OBJECT
 
@@ -11,8 +14,18 @@ public:
     UserInterface(QWidget *parent = Q_NULLPTR);
    
 public slots:
-    void On_Button_Clicked();
+    void OnButtonOpenServerClicked();
 private:
     Ui::UserInterfaceClass ui;
-    
+
+    BF::Server _server;
+
+    //---<Geerbt über ISocketListener>-----------------------------------------
+    virtual void OnConnectionLinked(int socketID) override;
+    virtual void OnConnectionListening(int socketID) override;
+    virtual void OnConnectionEstablished(int socketID) override;
+    virtual void OnConnectionTerminated(int socketID) override;
+    virtual void OnMessageSend(int socketID, const char* message, size_t messageSize) override;
+    virtual void OnMessageReceive(int socketID, const char* message, size_t messageSize) override;
+    //-------------------------------------------------------------------------
 };
