99aec8477a4419cc97eb2c94f7ca99b8f45b1d3a
BitPaw <NozsBitPaw@gmail.com>
2021-12-21 18:27:39 +0100

    LIB update

--- Program/Dependencies/include/Math/Geometry/Matrix4x4.hpp
+++ Program/Dependencies/include/Math/Geometry/Matrix4x4.hpp
@@ -5,9 +5,9 @@
 
 #include "AngleRadians.hpp"
 
-#include "../../../../Dependencies/include/glm/ext/matrix_transform.hpp"
-#include "../../../../Dependencies/include/glm/glm.hpp"
-#include "../../../../Dependencies/include/glm/ext.hpp"
+#include <GLM/glm.hpp>
+#include <GLM/ext.hpp>
+#include <GLM/ext/matrix_transform.hpp>
 
 #include <string>
 
--- Program/Dependencies/include/Math/Geometry/Vector3.hpp
+++ Program/Dependencies/include/Math/Geometry/Vector3.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../../../../Dependencies/include/glm/glm.hpp"
+#include <GLM/glm.hpp>
 #include "../Math.h"
 
 namespace BF
